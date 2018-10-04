ae4c83e653891bcd890bf769bb8ac852e56c302f
angelodel80 <angelodel80@gmail.com>
2018-10-04 21:56:38 +0200

    terza lezione elementi di linguaggi di marcatura e XML

--- slides/latex/beamer/corsoCodificaTesto/3-Lezione-elementi-xml/remove-files.sh
+++ slides/latex/beamer/corsoCodificaTesto/3-Lezione-elementi-xml/remove-files.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rm 3-elementi-XML.aux 3-elementi-XML.log 3-elementi-XML.nav 3-elementi-XML.out 3-elementi-XML.pdf 3-elementi-XML.snm 3-elementi-XML.toc
+rm 3-elementi-XML.aux 3-elementi-XML.log 3-elementi-XML.nav 3-elementi-XML.out 3-elementi-XML.pdf 3-elementi-XML.snm 3-elementi-XML.toc 3-elementi-XML.vrb texput.log
