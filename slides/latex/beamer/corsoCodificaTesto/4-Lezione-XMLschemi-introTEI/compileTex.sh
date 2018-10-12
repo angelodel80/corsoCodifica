5ccbb835dabcc87b185c2d33a2c6f4f1c371fcc5
angelodel80 <angelodel80@gmail.com>
2018-10-12 21:06:24 +0200

    working progress quarta lezione xsd

---
+++ slides/latex/beamer/corsoCodificaTesto/4-Lezione-XMLschemi-introTEI/compileTex.sh
@@ -0,0 +1,4 @@
+#! bin/bash
+pdflatex 4-Lezione-XMLschemi-introTEI.tex;
+pdflatex 4-Lezione-XMLschemi-introTEI.tex;
+pdflatex 4-Lezione-XMLschemi-introTEI.tex;
---
+++ slides/latex/beamer/corsoCodificaTesto/4-Lezione-XMLschemi-introTEI/remove-files.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+rm 4-Lezione-XMLschemi-introTEI.aux 4-Lezione-XMLschemi-introTEI.log 4-Lezione-XMLschemi-introTEI.nav 4-Lezione-XMLschemi-introTEI.out 4-Lezione-XMLschemi-introTEI.pdf 4-Lezione-XMLschemi-introTEI.snm 4-Lezione-XMLschemi-introTEI.toc 4-Lezione-XMLschemi-introTEI.vrb texput.log
