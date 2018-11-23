9d5cf3cafa83a00376537dad16abe3f7011f7d1d
angelodel80 <angelodel80@gmail.com>
2018-11-23 21:20:44 +0100

    updates

--- src/js/main-dom.js
+++ src/js/main-dom.js
@@ -4,6 +4,33 @@ function App() {
         console.log('run');
     };
     this.write = function (){
-        document.writeln('ciao!')
+        document.writeln('è attivo il modulo DOM per l\'XML? ', this.test_xml());
+        document.writeln('<br />');
+        document.writeln('è attivo il modulo Core 2.0? ', this.test_core2());
+        document.writeln('<br />');
+        document.writeln('è attivo il modulo Core 3.0? ', this.test_core3());
+        document.writeln('<br />');
     };
+
+    this.test_xml = function () {
+        var supportsDOM2XML = document.implementation.hasFeature('XML', 2.0);
+        console.log('XML 2.0', supportsDOM2XML);
+        return supportsDOM2XML?'SI':'NO';
+    }
+
+    this.test_core2 = function () {
+        var supportsDOM2Core = document.implementation.hasFeature('Core', 2.0);
+        console.log('Core 2.0', supportsDOM2Core);
+        return supportsDOM2Core?'SI':'NO';
+    }
+
+    this.test_core3 = function () {
+        var supportsDOM3Core = document.implementation.hasFeature('Core', 3.0);
+        console.log('Core 3.0', supportsDOM3Core);
+        return supportsDOM3Core?'SI':'NO';
+    }
+
+    
 }
+
+
